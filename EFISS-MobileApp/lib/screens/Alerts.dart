da75c9cbcaf42ef53ddd36f79fc4d4517a01a19e
samihahesham <61268414+samihahesham@users.noreply.github.com>
2021-03-31 02:13:01 +0200

    Update Alerts.dart

--- EFISS-MobileApp/lib/screens/Alerts.dart
+++ EFISS-MobileApp/lib/screens/Alerts.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+
+class NotificationView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Alerts"),
+        backgroundColor: Colors.lightBlue[900],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 32.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 22.0, left: 42.0, right: 42.0),
+                  child: Center(child: Text('No Alerts to show ')),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
